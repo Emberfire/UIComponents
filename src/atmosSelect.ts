17f0515a3826582ef60b64e0797fd4591f9ac2cd
Peter Petkov <petar6464@gmail.com>
2024-01-03 10:05:50 +0200

    Enable scroll-linked positioning

--- src/atmosSelect.ts
+++ src/atmosSelect.ts
@@ -330,25 +330,25 @@ export default class AtmosSelect {
             if (!AtmosSelect.openedSelect.hidden) AtmosSelect.openedSelect.positionMenuMock();
         });
 
-        // let initialRect: DOMRect;
-        // // Scroll-linked positioning for the menu mock. Disabled until a more elegant way is found to
-        // // position menu without causing so much reflow and triggering browser warnings.
-        // window.addEventListener("scroll", () => {
-        //     if (!AtmosSelect.openedSelect || AtmosSelect.openedSelect.hidden) return;
-        //
-        //     let buttonRect = AtmosSelect.openedSelect.buttonMock.getBoundingClientRect();
-        //     if (!initialRect) initialRect = buttonRect;
-        //
-        //     AtmosSelect.openedSelect.menuMock.style.transform =
-        //         `translate(${buttonRect.right - initialRect.right}px, ${buttonRect.bottom - initialRect.bottom}px)`;
-        // }, { capture: true, passive: true });
-        //
-        // window.addEventListener("scrollend", () => {
-        //     if (!AtmosSelect.openedSelect || AtmosSelect.openedSelect.hidden) return;
-        //
-        //     AtmosSelect.openedSelect.positionMenuMock();
-        //     initialRect = null;
-        // }, { capture: true, passive: true });
+        let initialRect: DOMRect;
+        // Scroll-linked positioning for the menu mock. Disabled until a more elegant way is found to
+        // position menu without causing so much reflow and triggering browser warnings.
+        window.addEventListener("scroll", () => {
+            if (!AtmosSelect.openedSelect || AtmosSelect.openedSelect.hidden) return;
+
+            let buttonRect = AtmosSelect.openedSelect.buttonMock.getBoundingClientRect();
+            if (!initialRect) initialRect = buttonRect;
+
+            AtmosSelect.openedSelect.menuMock.style.transform =
+                `translate(${buttonRect.right - initialRect.right}px, ${buttonRect.bottom - initialRect.bottom}px)`;
+        }, { capture: true, passive: true });
+
+        window.addEventListener("scrollend", () => {
+            if (!AtmosSelect.openedSelect || AtmosSelect.openedSelect.hidden) return;
+
+            AtmosSelect.openedSelect.positionMenuMock();
+            initialRect = null;
+        }, { capture: true, passive: true });
     }
 
     static get(element: HTMLSelectElement) {
